c1f4ab063437be3cb81e2bebbd6246b30f07e9e5
maheshwarishikha <maheshwari.shikha@gmail.com>
2020-07-16 15:20:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Kabanero CP4A!");
+  res.send("Hello from Kabanero CP4A 4.3!");
 });
  
 module.exports.app = app;
